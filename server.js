7e04514d4f62f36519224f4d9aa2dabe6b994194
Ben Bauer <benjaminbauer15@gmail.com>
2019-02-01 16:12:05 -0700

    small revisions

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.use(express.static(__dirname + '/client/dist/client'))
 app.use(bodyParser.json())
 app.use(bodyParser({extended : true}))
 
-const server = app.listen(5000, () => {
-    console.log('Listening on port 5000')
+const server = app.listen(4000, () => {
+    console.log('Listening on port 4000')
 })
 
 require('./server/config/routes')(app)
