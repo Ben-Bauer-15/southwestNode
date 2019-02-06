88584a346b9def3bdb540e1ac333e961a9182b94
Ben Bauer <benjaminbauer15@gmail.com>
2019-02-05 20:41:55 -0700

    latest commit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from "./http.service";
-import { FormControl } from "@angular/forms";
 
 
 @Component({
--- server/controllers/masterOfPuppets.js
+++ server/controllers/masterOfPuppets.js
@@ -3,51 +3,65 @@ const fs = require('fs')
 const http = require('http')
 const request = require('request')
 
-// example format for SWA URL: https://www.southwest.com/air/booking/select.html?adultPassengersCount=1&departureDate=2019-02-01&departureTimeOfDay=ALL_DAY&destinationAirportCode=BOS&fareType=USD&originationAirportCode=DEN&passengerType=ADULT&reset=false&returnDate=2019-02-04&returnTimeOfDay=ALL_DAY&seniorPassengersCount=0&tripType=roundtrip
+// example SWA URL: https://www.southwest.com/air/booking/select.html?adultPassengersCount=1&departureDate=2019-02-01&departureTimeOfDay=ALL_DAY&destinationAirportCode=BOS&fareType=USD&originationAirportCode=DEN&passengerType=ADULT&reset=false&returnDate=2019-02-04&returnTimeOfDay=ALL_DAY&seniorPassengersCount=0&tripType=roundtrip
 
 
 module.exports = {
 
-    grabSiteData : async function(req, res){
-        // console.log(req.body.adultsCount)
+    grabSiteData : function(req, res){
 
-        const urlToVisit = 'https://www.southwest.com/air/booking/select.html?adultPassengersCount='+ req.body.adultsCount +'&departureDate='+ req.body.departingDate +'&departureTimeOfDay=ALL_DAY&destinationAirportCode='+ req.body.destinationAirport +'&fareType=USD&originationAirportCode='+ req.body.originAirport +'&passengerType=ADULT&promoCode=&reset=true&returnDate='+ req.body.returningDate +'&returnTimeOfDay=ALL_DAY&seniorPassengersCount=0&tripType=roundtrip'
+        request.post('http://127.0.0.1:8000/validate', 
+            { form : 
+                {userEmail : req.body.userEmail,
+                userPhone : req.body.userPhone}},
 
-        // console.log(req.body.userEmail)
-        console.log(urlToVisit)
+            (err, response, body) => {
+                if (err){
+                    console.log(err)
+                    return false
+                }
+                else if (response.body == 'Form error'){
+                    res.json({message : response.body})
+                }
 
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage()
-        await page.goto(urlToVisit, {waitUntil: 'networkidle2'});
-        const SWcontent = await page.content()
-        await browser.close();
-
-        // fs.writeFile('./backend/files/test.txt', SWcontent, function(err){
-        //     if (err){
-        //         console.log(err)
-        //         return false
-        //     }
-        //     console.log("Successfully wrote the file")
-        // })
-
-        request.post('http://127.0.0.1:8000/startFareSearch', 
-            { form: {siteData : SWcontent, 
-                    userEmail : req.body.userEmail,
-                    userPhone : req.body.userPhone,
-                    originAirport : req.body.originAirport,
-                    destinationAirport : req.body.destinationAirport,
-                    departingDate : req.body.departingDate,
-                    returningDate : req.body.returningDate
-            } }, (err, response, body) => {
-            if (err){
-                console.log(err)
-                return false
-            }
-            // console.log(response.body)
-            res.json({message : response.body})
-            
-        })
+                else {
+                    browse(req, res)
+                }
+            })
 
     }
+}
+
+
+async function browse(req, res){
+    const urlToVisit = 'https://www.southwest.com/air/booking/select.html?adultPassengersCount='+ req.body.adultsCount +'&departureDate='+ req.body.departingDate +'&departureTimeOfDay=ALL_DAY&destinationAirportCode='+ req.body.destinationAirport +'&fareType=USD&originationAirportCode='+ req.body.originAirport +'&passengerType=ADULT&promoCode=&reset=true&returnDate='+ req.body.returningDate +'&returnTimeOfDay=ALL_DAY&seniorPassengersCount=0&tripType=roundtrip'
+
+    // console.log(req.body.userEmail)
+    console.log(urlToVisit)
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage()
+    await page.goto(urlToVisit, {waitUntil: 'networkidle2'});
+    const SWcontent = await page.content()
+    await browser.close();
+
+    request.post('http://127.0.0.1:8000/startFareSearch', 
+        { form: {siteData : SWcontent, 
+                userEmail : req.body.userEmail,
+                userPhone : req.body.userPhone,
+                originAirport : req.body.originAirport,
+                destinationAirport : req.body.destinationAirport,
+                departingDate : req.body.departingDate,
+                returningDate : req.body.returningDate
+        } }, (err, response, body) => {
+
+        if (err){
+            console.log(err)
+            return false
+        }
+        // console.log(response.body)
+        res.json({message : response.body})
+        
+    })
 }
 
